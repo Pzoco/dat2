e281dad8eaf29d1264395f418c6357d895611ea7
kralle@gmail.com <kralle@gmail.com@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-04-20 13:48:16 +0000

    Skriver videre - Gemmer bare for backup

--- trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Scanner.cs
+++ trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Scanner.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace WarSimulator_Handmade.SyntacticAnalyzer
+namespace WarSimulator_Handmade
 {
+    public enum TokenType
+    {
+        Identifier,
+        IntegerLiteral,
+
+        //Control Structures
+        If,
+        Else,
+        While,
+        
+        //Block Types
+        Team,
+        Regiment,
+        Behaviour,
+
+        //Signs
+        Slash,
+        LeftParen,
+        RightParen,
+        LeftBracket,
+        RightBracket,
+        Assignment,
+    }
     class Scanner
     {
+
+        public Scanner();
+
+        public bool IsGraphic(Char c);
+
+        public void ScanComment();
+        public TokenType Scan();
     }
 }
