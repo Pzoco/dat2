aa209a7932b3022e80f5adce7638e67f98331898
kralle@gmail.com <kralle@gmail.com@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-04-20 14:48:13 +0000

    Small changes oh yea

--- trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Token.cs
+++ trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Token.cs
@@ -5,7 +5,37 @@ using System.Text;
 
 namespace WarSimulator_Handmade
 {
+    public enum TokenType
+    {
+        Identifier,
+        IntegerLiteral,
+        Operator,
+
+        //Units
+        UnitFunction,
+        UnitStatName,
+
+        //Control Structures
+        If,
+        Else,
+        While,
+
+        //Block Types
+        Team,
+        Regiment,
+        Behaviour,
+
+        //Signs
+        Slash,
+        LeftParen,
+        RightParen,
+        LeftBracket,
+        RightBracket,
+        Assignment,
+    }
     class Token
     {
+        public TokenType type;
+        public string spelling;
     }
 }
