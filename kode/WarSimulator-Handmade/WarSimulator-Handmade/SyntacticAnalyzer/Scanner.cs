19ec910cd13a645f0bcf7ecb28c2edb6ea138e3e
mrmadpyro <mrmadpyro@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-04-26 12:15:28 +0000

    Tilføjet en klasse ved navn Source.cs, heri leger vi lidt med streamreader for at læse en ny karakter i filen Opdateret Scanner.cs klassen med vores fremgang hidtil. Derp.

--- kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Scanner.cs
+++ kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Scanner.cs
@@ -8,13 +8,34 @@ namespace WarSimulator_Handmade
     
     class Scanner
     {
+        private char CurrentChar;
+        private StringBuilder CurrentSpelling;
 
-        public Scanner() { }
+        private bool IsLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
 
-        public bool IsGraphic(Char c) { }
+        private bool IsDigit(char c)
+        {
+            return (c >= '0' && c <= '9');
+        }
 
-        public void ScanComment() { }
-        public TokenType ScanToken() { }
-        public Token Scan() { }
+        private bool IsOperator(char c)
+        {
+         return (c == '+' || c == '-' || c == '*' || c == '/' || c == '<' || c == '>' || c == '=' || c == '&' || c == '|');
+        }
+        private void take(char ExpectedChar)
+        {
+            if (CurrentChar == ExpectedChar)
+            {
+                CurrentSpelling.Append(CurrentChar);
+            }
+
+        }
+        private void takeIt()
+        {
+
+        }
     }
 }
