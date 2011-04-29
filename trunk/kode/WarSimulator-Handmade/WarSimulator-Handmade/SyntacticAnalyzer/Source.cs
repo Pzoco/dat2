28205f38716850c4e39c3a608505dcc08479d973
mrmadpyro <mrmadpyro@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-04-29 08:12:37 +0000

    Source = kode til at håndtere source file Små rettelser til Scanner.cs

--- trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Source.cs
+++ trunk/kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Source.cs
@@ -3,9 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace WarSimulator_Handmade.SyntacticAnalyzer
+namespace WarSimulator_Handmade
 {
     class Source
     {
+        public const char EndOfLine = '\n';
+        public const char EndOfText = '\u0000';
+
+        System.IO.FileInfo sourceFile;
+        System.IO.StreamReader source;
+        int currentLine;
+
+        public Source(String filename)
+        {
+            try
+            {
+                sourceFile = new System.IO.FileInfo(filename);
+                source = sourceFile.OpenText();
+                currentLine = 1;
+            }
+            catch
+            {
+                sourceFile = null;
+                source = null;
+                currentLine = 0;
+            }
+        }
+        internal char GetSource()
+        {
+            try
+            {
+                int c = source.Read();
+
+                if (c == -1)
+                {
+                    c = EndOfText;
+                }
+                else if (c == EndOfLine)
+                {
+                    currentLine++;
+                }
+                return (char)c;
+            }
+            catch (System.IO.IOException s)
+            {
+                return EndOfText;
+            }
+        }
+        internal int GetCurrentLine()
+        {
+            return currentLine;
+        }
     }
 }
