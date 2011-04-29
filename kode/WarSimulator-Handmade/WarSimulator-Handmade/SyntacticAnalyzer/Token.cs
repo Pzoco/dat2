6cc84a2a4d282d8e6f5fe06a063674c9cdffb2db
mrmadpyro <mrmadpyro@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-04-29 10:45:29 +0000

    CAPS-I-FIED AND ASSIGNED VALUES

--- kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Token.cs
+++ kode/WarSimulator-Handmade/WarSimulator-Handmade/SyntacticAnalyzer/Token.cs
@@ -7,57 +7,61 @@ namespace WarSimulator_Handmade
 {
     public enum TokenType
     {
-        Identifier,
-        IntegerLiteral,
-        Operator,
-        Position,
-        Regiments,
+        IDENTIFIER = 0,
+        INTEGERLITERAL = 1,
+        OPERATOR = 2,
+        POSITION = 3,
+        REGIMENTS = 4,
 
         //Units
-        UnitFunction,
-        UnitStatName,
+        UNITFUNCTION = 5,
+        UNITSTATNAME = 6,
 
         //Grid
-        GridStatName,
-        Width,
-        Height,
+        GRIDSTATNAME = 7,
+        WIDTH = 8,
+        HEIGHT = 9,
 
         //Maximums
-        MaximumsStatName,
-        Teams,
+        MAXIMUMSTATNAME = 10,
+        TEAMS = 11,
 
         //UnitStatName
-        Size, Range, Damage,
-        Movement, AttackSpeed, Health,
-        RegimentPosition,
-        Type,
+        SIZE = 12, 
+        RANGE = 13,
+        DAMAGE = 14,
+        MOVEMENT = 15,
+        ATTACKSPEED = 16,
+        HEALTH = 17,
+        REGIMENTPOSITION = 18,
+        TYPE = 19,
 
         //Attack Type
-        Melee,
-        Ranged,
+        MELEE = 20,
+        RANGED = 21,
 
         //Control Structures
-        If,
-        Else,
-        While,
+        IF = 22,
+        ELSE = 23,
+        WHILE = 24,
 
         //Block Types
-        Team,
-        Regiment,
-        Behaviour,
-        Config,
-        Grid,
-        Standards,
+        TEAM = 25,
+        REGIMENT = 26,
+        BEHAVIOUR = 27,
+        CONFIG = 28,
+        GRID = 29,
+        STANDARDS = 30,
 
         //Signs
-        Slash,
-        LeftParen,
-        RightParen,
-        LeftBracket,
-        RightBracket,
-        Comma,
-        SemiColon,
-        Assignment,
+        SLASH = 31,
+        LEFTPAREN = 32,
+        RIGHTPAREN = 33,
+        LEFTBRACKET = 34,
+        RIGHTBRACKET = 35,
+        COMMA = 36,
+        SEMICOLON = 37,
+        ASSIGNMENT = 38,
     }
     class Token
     {
