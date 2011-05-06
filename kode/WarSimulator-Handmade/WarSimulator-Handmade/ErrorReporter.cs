19f26fe25391cf405447daffe890708e55af8778
drfou070288@gmail.com <drfou070288@gmail.com@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-05-06 15:17:01 +0000

    De tre filer, som jeg manglede at uploade

---
+++ kode/WarSimulator-Handmade/WarSimulator-Handmade/ErrorReporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarSimulator_Handmade
+{
+    public class ErrorReporter
+    {
+        public int numErrors;
+
+        public ErrorReporter()
+        {
+            numErrors = 0;
+        }
+
+        public void ReportError(String message, String tokenName, SourcePosition pos)
+        {
+            Console.Write("ERROR: ");
+
+            for (int p = 0; p < message.Length; p++)
+                if (message[p] == '%')
+                    Console.Write(tokenName);
+                else
+                    Console.Write(message[p]);
+            Console.WriteLine(" " + pos.start + ".." + pos.finish);
+            numErrors++;
+        }
+
+        public void ReportRestriction(String message)
+        {
+            Console.WriteLine("RESTRICTION: " + message);
+        }
+    }
+}
---
+++ kode/WarSimulator-Handmade/WarSimulator-Handmade/TestClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarSimulator_Handmade
+{
+    class TestClass
+    {
+        static String objectName = "obj.tam";
+
+        private static Scanner scanner;
+        private static Parser parser;
+        private static ErrorReporter reporter;
+        private static TeamFile teamFile;
+        private static ConfigFile configFile;
+
+        static void Main(string[] args)
+        {
+            bool compiledOK;
+            String sourceName;
+
+            if (args.Length != 1)
+            {
+                sourceName = "C:\\Users\\Drfou\\Desktop\\kode.txt";
+                //Console.WriteLine("Usage: tc filename");
+                //Environment.Exit(1);
+            }
+            else
+            {
+                sourceName = args[0];
+            }
+            compiledOK = CompileProgram(sourceName, objectName);
+            Console.WriteLine("Parsing completed: " + compiledOK.ToString());
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+        static bool CompileProgram(String sourceName, String objectName)
+        {
+            Source source = new Source(sourceName);
+
+            if (source == null)
+            {
+                Console.WriteLine("Can't access source file " + sourceName);
+                Environment.Exit(1);
+            }
+
+            scanner = new Scanner(source);
+            reporter = new ErrorReporter();
+            parser = new Parser(scanner, reporter);
+
+            teamFile = parser.ParseTeamFile();
+
+            return true;
+        }
+    }
+}
