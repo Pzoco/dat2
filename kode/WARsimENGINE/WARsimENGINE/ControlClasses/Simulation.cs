9d696bc06d7d5a31f99f0c4d952e25202f0dd164
mrmadpyro <mrmadpyro@93a3053a-c20b-12b6-ca3f-9f1e0bdc6082>
2011-05-11 12:54:00 +0000

    Første udkast af strukturen på simulation engine

---
+++ kode/WARsimENGINE/WARsimENGINE/ControlClasses/Simulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WARsimENGINE.ControlClasses
+{
+    class Simulation
+    {
+        //StartSimluation()
+        //AdvanceTurn()
+        //EndSimulation()
+    }
+}
